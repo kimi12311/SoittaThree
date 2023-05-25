b5428bee5ddec7bf06bbd20e9875dc85531647b4
kimi12311 <kimianderssonmusic@gmail.com>
2023-05-25 22:12:09 +0200

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className=''>
+        <div className='text-center'>
+          <a target="_blank" className='flex items-center justify-center'>
+            <img src="/soitta.svg" className="logo scale-[3] mb-16" alt="Vite logo" />
+          </a>
+          <h2 className='text-3xl mb-12 font-semibold'>The Beginning of a New Era</h2>
+        </div>
+        <p className='pb-6 font-light'>
+          For now, here are the links:
         </p>
+        <a href='https://soittasoundworks.gumroad.com/l/nemesisvol1' className='text-white hover:text-red-300 transition-all duration-150 ease-in-out' target="_blank">Nemesis Vol.1</a>
+        <br/>
+        <br/>
+        <a href='https://soittasoundworks.gumroad.com/l/botanical' className='text-white hover:text-amber-200 transition-all duration-150 ease-in-out' target="_blank">Botanical for Xfer Serum</a>
+        <br/>
+        <br/>
+        <a href='https://soittasoundworks.gumroad.com/l/InterludePiano' className='text-white hover:text-blue-200 transition-all duration-150 ease-in-out' target="_blank">Interlude Piano</a>
+        <br/>
+        <br/>
+        <a href='https://soittasoundworks.gumroad.com/l/AuroraHouseEssentials' className='text-white hover:text-purple-300 transition-all duration-150 ease-in-out' target="_blank">Aurora House Essentials</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
